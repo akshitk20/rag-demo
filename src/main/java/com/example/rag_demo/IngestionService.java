5374ba2d5952ff72f9df11c9494577e60964c9f6
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-01-12 17:59:30 +0530

    completed ingestion of data

---
+++ src/main/java/com/example/rag_demo/IngestionService.java
@@ -0,0 +1,35 @@
+package com.example.rag_demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class IngestionService implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(IngestionService.class);
+    //this comes in because we have pg vector store on the class path autoconfiguration kicks in and bean of type
+    // vector store is wired up
+    private final VectorStore vectorStore;
+    @Value("classpath:/docs/article_thebeatoct2024.pdf")
+    private Resource marketPDF;
+    public IngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var pdfReader = new ParagraphPdfDocumentReader(marketPDF);
+        TextSplitter splitter = new TokenTextSplitter();
+        vectorStore.accept(splitter.apply(pdfReader.get()));
+        log.info("Vector store loaded with data");
+    }
+}
